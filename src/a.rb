4550ccb333238028bde941a33d76ee722430cd47
Koji Ota <afterjnih@gmail.com>
2022-03-22 16:12:07 +0900

    solve b

--- src/a.rb
+++ src/a.rb
@@ -1,6 +1,6 @@
 MAX = (10**5 -1)*(10**4)
 
-n, k = gets.chomp.split(' ').map(&:to_i)
+n = gets.chomp.to_i
 heights = gets.chomp.split(' ').map(&:to_i)
 heights = [0].concat(heights)
 total_costs = Array.new(n+1, MAX)
@@ -10,10 +10,9 @@ total_costs[1] = 0
 total_costs.each_with_index do |total_cost, idx|
   next if idx == 0
   next if idx == 1
-  (1..k).each do |plus|
-    break if idx - plus < 1
-    total_costs[idx] = [total_costs[idx], total_costs[idx-plus] + (heights[idx]-heights[idx-plus]).abs].min
-  end
+  total_costs[idx] = [total_costs[idx], total_costs[idx-1] + (heights[idx]-heights[idx-1]).abs, total_costs[idx-2] + (heights[idx]-heights[idx-2]).abs].min
 end
+
+# p total_costs
 
 puts total_costs[-1]
